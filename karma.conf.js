980ce045534ff5796e21938fe35da3bddd839f3a
Takkino31 <takkinoyaya@gmail.com>
2022-04-25 02:49:45 +0000

    Flux RSS done without modification

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular'),
+      dir: require('path').join(__dirname, './coverage/front-edacy'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+// @ts-ignore
+import xml2js from 'xml2js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular';
+  title = 'front-edacy';
+
+  public xmlItems: any;
+  p: number = 1;
+  constructor( private http: HttpClient ) {
+    this.loadXML();
+  }
+
+  loadXML()
+  {
+    /*Read Data*/
+    this.http.get('assets/lemonde.xml',
+      {
+        headers: new HttpHeaders()
+          .set('Content-Type', 'text/xml')
+          .append('Access-Control-Allow-Methods', 'GET')
+          .append('Access-Control-Allow-Origin', '*')
+          .append('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method"),
+        responseType: 'text'
+      })
+      // @ts-ignore
+      .subscribe((data) => {
+        this.parseXML(data)
+          // @ts-ignore
+          .then((data) => {
+            this.xmlItems = data;
+            this.xmlItems = this.xmlItems['rss']['channel']['0'];
+            // console.log(this.xmlItems.item)
+          });
+      });
+    /*Read Data*/
+  }
+  //store xml data into array variable
+  parseXML(data: any) {
+    return new Promise(resolve => {
+      let k: string | number,
+        arr: unknown = [],
+        parser = new xml2js.Parser(
+          {
+            trim: true,
+            explicitArray: true
+          }
+        );
+        parser.parseString(data, function (err: any, result:any) {
+          resolve(result);
+         });
+      });
+  }
 }
